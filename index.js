52e360c2f502ebe93c4c662fe7854d61b366fbf1
unknown <bkbagchi.dipto@gmail.com>
2025-07-09 13:08:01 +0600

    different types of routes added and described

--- index.js
+++ index.js
@@ -4,3 +4,23 @@ const app= express()
 app.listen(3000, ()=>{
     console.log("App is listening on 3000 port")
 })
+
+
+app.get("/", (req, res)=>{ //http://localhost:3000/
+    res.send("Hello, I'm sending response on 3000 port")
+})
+app.get("/about", (req, res)=>{
+    res.send("Ask me anything about me  ") //http://localhost:3000/about
+})
+app.get("/member/:memberid", (req, res)=>{ //http://localhost:3000/member/3
+    res.send(req.params)
+})
+app.get("/member/:member_id/book/:book_id", (req, res)=>{ //http://localhost:3000/member/3/book/18
+    res.send(req.params)
+})
+app.get("/member/:member_name-:member_id", (req, res)=>{ //http://localhost:3000/member/dipto-3
+    res.send(req.params)
+})
+app.get("/search", (req, res)=>{ //http://localhost:3000/search?name=dipto, http://localhost:3000/search?name=dipto&age=28
+    res.send(req.query)
+})
